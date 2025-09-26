8fb163250bd63b3bb09208c1b088a7db7e48b5ef
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-09-26 20:45:18 -0300

    add: new pathern colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,13 +1,20 @@
-import { red } from "@mui/material/colors";
+// import { red } from "@mui/material/colors";
 
 // General colors
 export const COLORS = {
-    black: '#000000ff',
     dark: 'rgba(42, 43, 46, 1)',
     grey: 'rgba(90, 90, 102, 1)',
     darkGrey: 'rgba(164, 194, 168, 1)',
     lightGrey: 'rgba(172, 235, 152, 1)',
     red: '#f93030ff',
+
+     background: 'rgb(249, 245, 240)',   // 60% - Fondo base
+    surface:    'rgb(242, 234, 211)',   // 30% - Tarjetas, paneles
+    accent:     'rgb(244, 153, 26)',    // 10% - Botones, highlights
+    text:       'rgb(52, 79, 31)',      // Texto principal
+    white:      '#FFFFFF',              // Neutro: fondos / texto sobre oscuro
+    black:      '#000000',              // Neutro: texto / máximo contraste
+
 };
 
 // NutriScore colors
